d4abb9c79822c1d472a1e9465957895badddd267
Smit Mistry <smitmistry19@gmail.com>
2023-05-28 07:51:22 +0530

    javascript modified

--- main.js
+++ main.js
@@ -18,9 +18,16 @@ document.addEventListener("click", handleShareButtonClick);
 
 function handleShareButtonClick(e) {
   // Check if the clicked element has the id "share-btn" or "share-btn--active"
-  if (e.target.id === "share-btn" || e.target.id === "share-btn--active") {
+  // if (e.target.id === "share-btn" || e.target.id === "share-btn--active") {
+  //   // Toggle the "activated" class on the element with id "share--active"
+  //   const active = document.getElementById("share--active");
+  //   active.classList.toggle("activated");
+  // }
+
+  if (e.target.closest('#share-btn, #share-btn--active')) {
     // Toggle the "activated" class on the element with id "share--active"
     const active = document.getElementById("share--active");
     active.classList.toggle("activated");
   }
+  
 }
